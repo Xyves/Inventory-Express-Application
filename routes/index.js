03784975711d99090e9e8724240b5f99d927b854
Xyves <xodrixx@protonmail.com>
2024-07-06 09:20:58 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.redirect("/catalog");
+  console.log("Title:", title);
+  console.log("Game count:", game_count);
+  console.log("Game instance count:", game_instance_count);
 });
 
 module.exports = router;
